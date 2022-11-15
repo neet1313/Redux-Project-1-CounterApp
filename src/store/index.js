b44b774056863e3047faea53a35e342b6b0a015a
Prabhneet Singh <prabhneet13@gmail.com>
2022-11-15 23:35:35 +0530

    connected redux

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux'
+const counterReducer = (state = { counter: 0 }, action) => {
+    switch (action.type) {
+        case "increment":
+            return {
+                counter: state.counter + 1
+            }
+
+        case "decrement":
+            return {
+                counter: state.counter - 1
+            }
+
+        default:
+            return state
+    }
+}
+
+const store = createStore(counterReducer);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
+
+export default store;
